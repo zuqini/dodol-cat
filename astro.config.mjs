5183eeff0f9ab25fcd68780a5374b43f8c113c06
zuqini <lizuqiliang@gmail.com>
2024-12-10 16:29:11 -0800

    react integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import { defineConfig } from 'astro/config';
 
 import icon from 'astro-icon';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()]
+  integrations: [icon(), react()]
 });
